c393fd8bc0580923226abd319168429a4cb674c0
Chadwain <chadwainholness@gmail.com>
2021-05-12 21:01:09 -0400

    improve build.zig delete test/tests.zig

--- build.zig
+++ build.zig
@@ -2,76 +2,74 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const Pkg = std.build.Pkg;
 
-const harfbuzzPkg = Pkg{
-    .name = "harfbuzz",
-    .path = "dependencies/harfbuzz.zig",
-};
-const freetypePkg = Pkg{
-    .name = "freetype",
-    .path = "dependencies/freetype.zig",
-    .dependencies = &[_]Pkg{harfbuzzPkg},
-};
-const SDL2Pkg = Pkg{
-    .name = "SDL2",
-    .path = "dependencies/SDL2.zig",
-};
-const zssPkg = Pkg{
-    .name = "zss",
-    .path = "zss.zig",
-    // TODO remove freetype dependency
-    .dependencies = &[_]Pkg{ harfbuzzPkg, SDL2Pkg, freetypePkg },
+const pkgs = struct {
+    const harfbuzz = Pkg{
+        .name = "harfbuzz",
+        .path = "dependencies/harfbuzz.zig",
+    };
+    const freetype = Pkg{
+        .name = "freetype",
+        .path = "dependencies/freetype.zig",
+        .dependencies = &[_]Pkg{harfbuzz},
+    };
+    const SDL2 = Pkg{
+        .name = "SDL2",
+        .path = "dependencies/SDL2.zig",
+    };
+    const zss = Pkg{
+        .name = "zss",
+        .path = "zss.zig",
+        .dependencies = &[_]Pkg{ harfbuzz, SDL2, freetype },
+    };
 };
 
-const c_includes = [_][]const u8{
-    "dependencies/SDL/include",
-    "dependencies/SDL_image",
-    "dependencies/freetype/include",
-    "dependencies/harfbuzz/src",
+pub const graphical_tests = blk: {
+    const Test = struct {
+        name: []const u8,
+        root: []const u8,
+    };
+
+    break :blk [_]Test{
+        Test{ .name = "block_format", .root = "test/block_formatting.zig" },
+        Test{ .name = "inline_format", .root = "test/inline_formatting.zig" },
+    };
 };
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+
     const lib = b.addStaticLibrary("zss", "zss.zig");
     lib.setBuildMode(mode);
+    lib.setTarget(target);
     lib.install();
 
     var main_tests = b.addTest("zss.zig");
     main_tests.setBuildMode(mode);
-    main_tests.addPackage(harfbuzzPkg);
-    main_tests.addPackage(freetypePkg);
-    main_tests.addPackage(SDL2Pkg);
-    main_tests.addPackage(zssPkg);
-    main_tests.linkSystemLibrary("c");
+    main_tests.setTarget(target);
+    main_tests.addPackage(pkgs.harfbuzz);
+    main_tests.linkLibC();
     main_tests.linkSystemLibrary("harfbuzz");
-    main_tests.linkSystemLibrary("freetype");
-    main_tests.linkSystemLibrary("SDL2");
-    for (c_includes) |include| {
-        main_tests.addIncludeDir(include);
-    }
-    main_tests.addLibPath("lib");
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 
     const graphical_test_step = b.step("graphical-test", "Run graphical tests");
-    const all_tests = @import("test/tests.zig");
-    inline for (all_tests.tests) |t| {
-        var test_exec = b.addExecutable(t.name, "test/" ++ t.root);
+    inline for (graphical_tests) |t| {
+        var test_exec = b.addExecutable(t.name, t.root);
         test_exec.setBuildMode(mode);
-        test_exec.addPackage(freetypePkg);
-        test_exec.addPackage(harfbuzzPkg);
-        test_exec.addPackage(SDL2Pkg);
-        test_exec.addPackage(zssPkg);
-        test_exec.linkSystemLibrary("c");
-        test_exec.linkSystemLibrary("freetype");
+        test_exec.setTarget(target);
+        test_exec.addPackage(pkgs.harfbuzz);
+        test_exec.addPackage(pkgs.freetype);
+        test_exec.addPackage(pkgs.SDL2);
+        test_exec.addPackage(pkgs.zss);
+        test_exec.linkLibC();
         test_exec.linkSystemLibrary("harfbuzz");
+        test_exec.linkSystemLibrary("freetype");
         test_exec.linkSystemLibrary("SDL2");
         test_exec.linkSystemLibrary("SDL2_image");
-        for (c_includes) |include| {
-            test_exec.addIncludeDir(include);
-        }
-        test_exec.addLibPath("lib");
         test_exec.install();
+
         graphical_test_step.dependOn(&test_exec.step);
     }
 }
--- test/tests.zig
+++
@@ -1,9 +0,0 @@
-pub const Test = struct {
-    name: []const u8,
-    root: []const u8,
-};
-
-pub const tests = [_]Test{
-    Test{ .name = "block_format", .root = "block_formatting.zig" },
-    Test{ .name = "inline_format", .root = "inline_formatting.zig" },
-};
